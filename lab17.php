a9af458c2a9f75c00d3fd0261f52590b39975ad9
AlvaroDera <alvaro.alvarez@utp.ac.pa>
2023-09-04 20:22:21 -0500

    lab

---
+++ lab17.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> laboratorio 1.7 </title>
+</head>
+<body>
+    <?php
+    $x = 24;
+    $pi = 3.1416;
+    $animal = "conejo";
+    $saludo = "Hola Caracola";
+    echo $x, "<br>", $pi, "<br>", $animal, "<br>", $saludo;
+    ?>
+</body>
+</html>
