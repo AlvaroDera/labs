5a5b7a1968c49c0d531b32bfa3c9150e4aa3bede
AlvaroDera <alvaro.alvarez@utp.ac.pa>
2023-09-04 20:26:16 -0500

    lab

---
+++ lab18.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> laboratorio 1.8 </title>
+</head>
+<body>
+    <?php
+    $a = 8;
+    $b = 3;
+    echo $a + $b, "<br>";
+    echo $a - $b, "<br>";
+    echo $a * $b, "<br>";
+    echo $a / $b, "<br>";
+    $a++; echo $a, "<br>";
+    $b--; echo $b, "<br>";
+    ?>
+</body>
+</html>
