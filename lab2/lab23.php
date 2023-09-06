35f590eb0b9450ce4bdc996b18ae51891b03bbec
AlvaroDera <alvaro.alvarez@utp.ac.pa>
2023-09-05 21:47:08 -0500

    lab

---
+++ lab2/lab23.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> laboratorio 2.3 </title>
+</head>
+<body>
+    <center>
+        <?php
+            print ("<ul>\n");
+            $i=1;
+            while ($i <= 5)
+            {
+                print ("<li>Elementos $i</li>n");
+                $i++;
+            }
+            print ("</ul>\n");
+        ?>
+    </center>
+</body>
+</html>
