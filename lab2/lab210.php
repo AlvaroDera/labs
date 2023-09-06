776842b900fd0da07ff8474323f33c2db967aa3c
AlvaroDera <alvaro.alvarez@utp.ac.pa>
2023-09-05 22:39:26 -0500

    lab

---
+++ lab2/lab210.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> laboratorio 2.10 </title>
+</head>
+<body>
+    <?php
+    $persona = array (
+        array( "nombre" => "Rosa", "estatura" => 168, "sexo" => "F"),
+        array( "nombre" => "Ignacio", "estatura" => 175, "sexo" => "M"),
+        array( "nombre" => "Daniel", "estatura" => 172, "sexo" => "M"),
+        array( "nombre" => "Ruben", "estatura" => 182, "sexo" => "M"),
+    );
+
+    echo "<b>Datos Sobre El Personal<b><br><hr>";
+
+    $numPersonas = count($persona);
+
+    for ($i = 0; $i < $numPersonas; $i++) {
+        echo "Nombre: <b>", $persona[$i]['nombre'], "</b><br>";
+        echo "Estatura: <b>", $persona[$i]['estatura'], " cm</b><br>";
+        echo "Sexo: <b>", $persona[$i]['sexo'], "</b><br><hr>";
+    }
+    ?>
+</body>
+</html>
