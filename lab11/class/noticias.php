1c2bcc50c4d13f7e5ac26bbbb3f3838e4122645b
AlvaroDera <alvaro.alvarez@utp.ac.pa>
2023-10-12 23:28:16 -0500

    lab

--- lab11/class/noticias.php
+++ lab11/class/noticias.php
@@ -1,48 +1,49 @@
 <?php
 
-	require_once('modelo.php');
+require_once('modelo.php');
 
-	class noticia extends modeloCredencialesBD {
-		protected $titulo;
-		protected $texto;
-		protected $categoria;
-		protected $fecha;
-		protected $imagen;
+class noticias extends modeloCredencialesBD {
+    protected $titulo;
+    protected $texto;
+    protected $categoria;
+    protected $fecha;
+    protected $imagen;
 
-		public function __construct() {
-			parent::__construct();
-		}
+    public function __construct() {
+        parent::__construct();
+    }
 
-		public function consultar_noticias() {
-			$instruccion = "CALL sp_listar_noticias()";
+    public function consultar_noticias() {
+        $instruccion = "CALL sp_listar_noticias()";
 
-			$consulta=$this->_db->query($instruccion);
-			$resultado=$consulta->fetch_all(MYSQLI_ASSOC);
+        $consulta = $this->_db->query($instruccion);
+        $resultado = $consulta->fetch_all(MYSQLI_ASSOC);
 
-    		if(!$resultado){
-				echo "Fallo al consultar las noticias";
-			}
-			else{
-				return $resultado;
-				$resultado->close();
-				$this->_db->close();
-			}
-		}
+        if (!$resultado) {
+            echo "Fallo al consultar las noticias";
+        }
 
-		public function consultar_noticias_5($limite) {
-			$instruccion = "CALL sp_listar_noticias_5('".$limite."')";
+        $consulta->close(); // Cierra la consulta
+        $this->_db->close(); // Cierra la conexión a la base de datos
 
-			$consulta=$this->_db->query($instruccion);
-			$resultado=$consulta->fetch_all(MYSQLI_ASSOC);
+        return $resultado;
+    }
 
-    		if(!$resultado) {
-				echo "Fallo al consultar las noticias";
-			}
-			else {
-				return $resultado;
-				$resultado->close();
-				$this->_db->close();
-			}
-		}
-	}
+    public function consultar_noticias_5($limite) {
+        $instruccion = "CALL sp_listar_noticias_5('".$limite."')";
+
+        $consulta = $this->_db->query($instruccion);
+        $resultado = $consulta->fetch_all(MYSQLI_ASSOC);
+
+        if (!$resultado) {
+            echo "Fallo al consultar las noticias";
+        }
+
+        $consulta->close(); // Cierra la consulta
+        $this->_db->close(); // Cierra la conexión a la base de datos
+
+        return $resultado;
+    }
+}
+
 ?>
